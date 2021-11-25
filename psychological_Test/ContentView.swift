ea3701c018d9661e869ba5af59d04dffdc9e2f8a
Gordon Chou <chouguting@gmail.com>
2021-11-25 14:54:14 +0800

    Initial Commit

---
+++ psychological_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  psychological_Test
+//
+//  Created by 周固廷 on 2021/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ psychological_Test/psychological_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  psychological_TestApp.swift
+//  psychological_Test
+//
+//  Created by 周固廷 on 2021/11/25.
+//
+
+import SwiftUI
+
+@main
+struct psychological_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
